5e2e71d90923a571e12ea10385b46501168317dc
FedeYozora <federicobonfi@gmail.com>
2024-02-23 12:33:00 +0100

    added JWTAuthFilter and Config

--- src/main/java/it/epicode/Gestione/Eventi/security/SecurityConfig.java
+++ src/main/java/it/epicode/Gestione/Eventi/security/SecurityConfig.java
@@ -1,4 +1,34 @@
 package it.epicode.Gestione.Eventi.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private JWTAuthFilter jwtAuthFilter;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.formLogin(AbstractHttpConfigurer::disable);
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        httpSecurity.authorizeHttpRequests(request -> request.requestMatchers("/**").permitAll());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    PasswordEncoder getBCrypt() {
+        return new BCryptPasswordEncoder(11);
+    }
 }
