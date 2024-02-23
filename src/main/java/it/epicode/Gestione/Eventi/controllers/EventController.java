0f1dc4571e224339f6b38e902973b2587e74c1f3
FedeYozora <federicobonfi@gmail.com>
2024-02-23 16:33:07 +0100

    fixed authorization

--- src/main/java/it/epicode/Gestione/Eventi/controllers/EventController.java
+++ src/main/java/it/epicode/Gestione/Eventi/controllers/EventController.java
@@ -1,15 +1,18 @@
 package it.epicode.Gestione.Eventi.controllers;
 
 import it.epicode.Gestione.Eventi.entities.Event;
+import it.epicode.Gestione.Eventi.entities.User;
 import it.epicode.Gestione.Eventi.payloads.NewEventDTO;
 import it.epicode.Gestione.Eventi.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/events")
@@ -48,5 +51,11 @@ public class EventController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void findByIdAndDelete(@PathVariable Integer id) {
         eventService.findByIdAndDelete(id);
+    }
+
+    @GetMapping("/myEvents")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Event> getMyEvents(@AuthenticationPrincipal User currentUser) {
+        return currentUser.getEvents();
     }
 }
--- src/main/java/it/epicode/Gestione/Eventi/security/SecurityConfig.java
+++ src/main/java/it/epicode/Gestione/Eventi/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package it.epicode.Gestione.Eventi.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -10,9 +11,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Autowired
     private JWTAuthFilter jwtAuthFilter;
@@ -22,6 +25,8 @@ public class SecurityConfig {
         httpSecurity.formLogin(AbstractHttpConfigurer::disable);
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
         httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        httpSecurity.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         httpSecurity.authorizeHttpRequests(request -> request.requestMatchers("/**").permitAll());
         return httpSecurity.build();
