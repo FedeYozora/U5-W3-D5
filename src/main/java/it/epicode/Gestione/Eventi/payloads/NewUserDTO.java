c5aa6901364ff1490b593548a7205fbfc280be97
FedeYozora <federicobonfi@gmail.com>
2024-02-23 14:00:06 +0100

    fixes to filter

--- src/main/java/it/epicode/Gestione/Eventi/payloads/NewUserDTO.java
+++ src/main/java/it/epicode/Gestione/Eventi/payloads/NewUserDTO.java
@@ -2,10 +2,14 @@ package it.epicode.Gestione.Eventi.payloads;
 
 import it.epicode.Gestione.Eventi.entities.Event;
 import it.epicode.Gestione.Eventi.enums.Role;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
 public record NewUserDTO(
+        @NotNull(message = "Name obbligatorio")
+        @Size(min = 4, message = "The Name should be of at least 2 characters")
         String name,
         String surname,
         String username,
