a09caf4355a2ec00d5ec84db5bda605055c6542c
FedeYozora <federicobonfi@gmail.com>
2024-02-23 10:31:58 +0100

    Initial commit

---
+++ src/test/java/it/epicode/Gestione/Eventi/U5W3D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.Gestione.Eventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
